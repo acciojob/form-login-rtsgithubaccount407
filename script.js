6c03d75e6e41214f5fd2ea80733184335471569b
Rohit Sharma <123295893+rtsgithubaccount407@users.noreply.github.com>
2023-08-29 18:53:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
 	let fname= document.querySelector('input[name="fname"]').value;
-	let lname= document.querySelector('input[type="lname"]').value;
+	let lname= document.querySelector('input[name="lname"]').value;
 alert(fname +" "+lname);
 }
